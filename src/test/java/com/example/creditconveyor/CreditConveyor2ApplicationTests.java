0eb1ce4149088e811ca5b4475ef747f55c8d905b
woda23 <53645168+woda23@users.noreply.github.com>
2023-08-02 00:50:19 +0300

    fix: change name

--- src/test/java/com/example/creditconveyor/CreditConveyor2ApplicationTests.java
+++ src/test/java/com/example/creditconveyor/CreditConveyor2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CreditConveyor2ApplicationTests {
+class CreditConveyorApplicationTests {
 
     @Test
     void contextLoads() {
